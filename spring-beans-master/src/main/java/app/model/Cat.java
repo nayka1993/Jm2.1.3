4640e51b894277394c45a064966339c8aff9c4f3
nayka1993 <cykaa95@mail.ru>
2020-06-30 14:54:48 +0300

    Sea

---
+++ spring-beans-master/src/main/java/app/model/Cat.java
@@ -0,0 +1,11 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("Cat")
+public class Cat extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Cat";
+    }
+}
---
+++ spring-beans-master/src/main/java/app/model/Timer.java
@@ -0,0 +1,15 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("Timer")
+public class Timer extends Animal {
+
+    private Long nanoTime = System.nanoTime();
+
+
+    public Long getTime() {
+        return nanoTime;
+    }
+
+}
